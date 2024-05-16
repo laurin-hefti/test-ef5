f074c8acb3ed2ea7d1f95de70f7cec033d68f023
laurin-hefti <96139452+laurin-hefti@users.noreply.github.com>
2024-05-16 13:48:38 +0200

    Update Fluid_dynamics.cpp

--- Fluid_dynamics.cpp
+++ Fluid_dynamics.cpp
@@ -11,6 +11,7 @@ class Cell {
   int x;
   int y;
   double preasure;
+  TwoNum dx; 
   
   double radius = 10;
   
@@ -26,7 +27,7 @@ public:
       this->preasure = start_p;
   }
   
-   TwoNum calculate_d(Cell* store, int len){
+   void calculate_d(Cell* store, int len){
        double f[2] = {0,0};
        
         for (int i = 0; i < len; i++){
@@ -35,19 +36,33 @@ public:
             {
                 if (store[i].preasure < this->preasure){
                     TwoNum dir = {0,0};
-                    return dir;
+                    this->dx = dir;
                 } //keine saugwirkung der zelle
                 
                 int d_x = store[i].x - this->x;
                 int d_y = store[i].y - this->y;
                 double dif = abs(this->preasure-store[i].preasure);
                 
-                f[0] += d_x * 1/(d*d) * dif;
+                f[0] += d_x * 1/(d*d) * dif; //not dist
                 f[1] += d_y * 1/(d*d) * dif;
             }
         }
         TwoNum dir = {f[0], f[1]};
-        return dir;
+        this->dx = dir;
+  }
+  
+  void apply_pres(Cell* store, int len){
+      int nearest_cell = 0;
+      double dist_to_cell = getD(store[i].x, store[i].y);
+      
+      for (int i = 0; i < len; i++){
+          if (getD(this->dx.x, this->dx.y) < dist_to_cell){
+              nearest_cell = i;
+              dist_to_cell = getD(this->dx.x, this->dx.y);
+          }
+      }
+      
+      store[i].preasure += 
   }
 };
 

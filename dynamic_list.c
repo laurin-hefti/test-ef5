47db7e0310b1c4d2221a000305d66da111ac74db
laurin-hefti <96139452+laurin-hefti@users.noreply.github.com>
2024-05-25 17:32:43 +0200

    Update dynamic_list.c

--- dynamic_list.c
+++ dynamic_list.c
@@ -2,13 +2,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //external definition
-#define data_name d64
-#define data_type double
+#define data_name i64
+#define data_type int
 
 //start file
 
+//usefule print not finish
+#define print(cont) printf(#cont)
 //default better error
 #ifndef data_name
 #define data_name d64
@@ -44,17 +47,40 @@ data_name newObj()
     return obj;
 }
 
+int checkIfFull(data_name* obj)
+{
+    if (obj->len + 1 >= obj->size){
+        return 1;
+    }
+    return 0;
+}
+
+void copyContent(data_name* obj)
+{
+    data_type* list = createList(obj->size*2);
+    memcpy(list, obj->list, obj->size*sizeof(data_type));
+    free(obj->list);
+    obj->list = list;
+    obj->size *= 2;
+}
+
 void addObj(data_name* obj, data_type d)
 {
+    if (checkIfFull(obj))
+    {
+        copyContent(obj);
+    }
     obj->list[obj->len] = d;
     obj->len += 1;
     //def if it is pointer
 }
 
+
 int main(){
-    d64 list = newd64();
-    addd64(&list, 4);
-    addd64(&list, 6);
-    printf("%d", list.list[1]);
+    i64 list = newi64();
+    for (int i = 0; i < 30; i++){
+        addi64(&list, i);
+    }
+    printf("%i ", list.list[24]);
     return 0;
 }
